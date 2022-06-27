b68e808fcff34956b13465e17f056eaf1fc4b01d
yinan <demoshuwang@gmail.com>
2022-06-27 15:55:47 +0800

    feat: add keepalive params

--- client/client.go
+++ client/client.go
@@ -8,14 +8,22 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/metadata"
 
 	pb "grpc-demo/api/user"
 )
 
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             1 * time.Second,  // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
 func main() {
 	fmt.Println("client start")
-	conn, err := grpc.Dial("127.0.0.1:8012", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	// 通过设置超时时间，然后抓包之后可以明确看到，超时时间和链接的复用存在着一定关系
+	conn, err := grpc.Dial("127.0.0.1:8012", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock(), grpc.WithKeepaliveParams(kacp))
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
@@ -27,7 +35,7 @@ func main() {
 	}
 	con := metadata.NewOutgoingContext(context.Background(), md)
 	//con := metadata.NewIncomingContext(context.Background(), md)
-	ctx, cancel := context.WithTimeout(con, time.Second)
+	ctx, cancel := context.WithTimeout(con, 60*time.Second)
 	defer cancel()
 	md1, _ := metadata.FromIncomingContext(con)
 	fmt.Println(md1)
@@ -49,7 +57,9 @@ func main() {
 		log.Fatalf("create user failed: %v", err)
 	}
 
-	log.Printf("create user success: %v", user)
+	log.Printf("create user success: %v\n", user)
+
+	time.Sleep(30 * time.Second)
 
 	// 获取用户信息 GetUser
 	getUserRequest := &pb.GetUserRequest{
@@ -59,6 +69,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("get user failed: %v", err)
 	}
-	fmt.Printf("user: %v", getUser)
+	log.Printf("get user success: %v\n", getUser)
 
 }
--- server/server.go
+++ server/server.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 
@@ -19,6 +21,17 @@ var (
 	scheme      = "user"
 	serviceName = "gprc.demo.user"
 	addr        = "127.0.0.1:8012"
+	kaep        = keepalive.EnforcementPolicy{
+		MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+		PermitWithoutStream: true,            // Allow pings even when there are no active streams
+	}
+	kasp = keepalive.ServerParameters{
+		MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+		MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+		MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+		Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+		Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+	}
 )
 
 type User struct {
@@ -61,6 +74,8 @@ func main() {
 			UserServerInterceptor,
 			WorldInterceptor,
 		)),
+		grpc.KeepaliveParams(kasp),
+		grpc.KeepaliveEnforcementPolicy(kaep),
 	}
 	s := grpc.NewServer(opts...)
 	pb.RegisterUserServiceServer(s, &User{})
